773cadf40056c98deacd73ceb64dfbd425d5d5e1
cristhro <cristhro@ucm.es>
2017-03-08 21:54:47 +0100

    Udate and Migrate from bitbucket

---
+++ config.js
@@ -0,0 +1,17 @@
+﻿module.exports = {
+ // Nombre de la base de datos de vuestra
+ // práctica
+ dbName: "entregaSaboteur",
+
+ // Nombre de usuario que utilizáis para acceder a la BD.
+ dbUser: "a",
+
+ // Contraseña que utilizáis para acceder a la BD
+ dbPassword: "a",
+
+ // Host donde está la BD.
+ dbHost: "ec2-54-172-228-97.compute-1.amazonaws.com",
+
+ // Puerto en el que escucha vuestra aplicación
+ port: 3000
+};
